aacfbea5e0b614795f4668e7025057ae976f4006
Anderson Roberto de Oliveira <andersonn.roberto@gmail.com>
2024-12-30 09:07:13 -0300

    feat: Example of showing validation error from backend

--- src/livrosapp.client/src/app/assuntos-add/assuntos-add.component.ts
+++ src/livrosapp.client/src/app/assuntos-add/assuntos-add.component.ts
@@ -6,20 +6,30 @@ import { AssuntosService } from '../assuntos/assuntos.service';
 @Component({
   selector: 'app-assuntos-add',
   standalone: false,
-  
+
   templateUrl: './assuntos-add.component.html',
-  styleUrl: './assuntos-add.component.css'
+  styleUrl: './assuntos-add.component.css',
 })
 export class AssuntosAddComponent {
-
   descricao = new FormControl('');
 
-  constructor(private readonly assuntosService: AssuntosService, private readonly router: Router) { }
+  constructor(
+    private readonly assuntosService: AssuntosService,
+    private readonly router: Router
+  ) {}
 
   addAssunto() {
-    this.assuntosService.addAssunto({ descricao: this.descricao.value })
-      .subscribe(() => {
-        this.router.navigate(['/assuntos']);
+    this.assuntosService
+      .addAssunto({ descricao: this.descricao.value })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/assuntos']);
+        },
+        error: (error) => {
+          let key = Object.keys(error.error.errors)[0];
+          let value = error.error.errors[key][0];
+          window.alert(value);
+        },
       });
   }
 }
